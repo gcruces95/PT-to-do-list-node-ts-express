115a78d0fa09846957b967fc77194682a01c0aa8
gcruces95 <gabrielcrucesg@gmail.com>
2025-05-30 16:42:40 -0400

    feat: :sparkles: actualiza configuración de variables de entorno
    - Se añaden las variables de entorno DB_PASSWORD y NODE_ENV con valores predeterminados.
    - Se mejora la organización y legibilidad del archivo envs.ts al agregar espacios en blanco.

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -4,11 +4,14 @@ import { get } from 'env-var';
 export const envs = {
   PORT: get('PORT').default(3000).asPortNumber(),
   PUBLIC_PATH: get('PUBLIC_PATH').default('public').asString(),
+
   DB_URL: get('DB_URL').required().asString(),
+
   DB_PASSWORD: get('DB_PASSWORD').required().asString(),
   DB_NAME: get('DB_NAME').required().asString(),
   DB_HOST: get('DB_HOST').required().asString(),
   DB_PORT: get('DB_PORT').required().asPortNumber(),
   DB_USERNAME: get('DB_USERNAME').required().asString(),
+  
   NODE_ENV: get('NODE_ENV').default('development').asString(),
 };
