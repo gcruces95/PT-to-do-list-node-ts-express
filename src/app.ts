75b00f82564a42851cfc4080e1adae346dc96da7
gcruces95 <gabrielcrucesg@gmail.com>
2025-05-30 14:59:15 -0400

    feat: :sparkles: conecta la base de datos en la función principal
    - Añade la función connectDB para establecer la conexión a la base de datos PostgreSQL al iniciar la aplicación.
    - Cambia el puerto predeterminado de 4000 a 3000 en la configuración del servidor.

--- src/app.ts
+++ src/app.ts
@@ -1,26 +1,23 @@
 import { Server } from "./presentation/server";
 import { envs } from "./config/envs";
 import { AppRoutes } from "./presentation/routes";
-
-
+import { connectDB } from "./data/postgres/database";
 
 (async () => {
     main();
 })();
 
-
-function main() {
-
-
+async function main() {
+    // Conectar a la base de datos
+    await connectDB();
 
     const server = new Server(
         {
-            port: envs.PORT || 4000,
+            port: envs.PORT || 3000,
             public_path: envs.PUBLIC_PATH,
             routes: AppRoutes.routes,
         }
     );
 
     server.start();
-
 }
