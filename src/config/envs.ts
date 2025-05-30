c9df9c8e93b76fdd3feb52227e67a8acc6b66d28
gcruces95 <gabrielcrucesg@gmail.com>
2025-05-30 14:50:52 -0400

    feat: :sparkles: actualiza configuración de variables de entorno
    - Modifica el archivo envs.ts para establecer valores predeterminados para PORT y NODE_ENV.
    - Reorganiza las variables de entorno para mejorar la legibilidad y consistencia.

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -1,18 +1,14 @@
 import 'dotenv/config';
 import { get } from 'env-var';
 
-
 export const envs = {
-
-    PORT: get('PORT').required().asPortNumber(),
-    PUBLIC_PATH: get('PUBLIC_PATH').default('public').required().asString(),
-
-    DB_URL: get('DB_URL').required().asString(),
-    DB_HOST: get('DB_HOST').required().asString(),
-    DB_USERNAME: get('DB_USERNAME').required().asString(),
-    DB_NAME: get('DB_NAME').required().asString(),
-    DB_PORT: get('DB_PORT').required().asPortNumber(),
-    DB_PASSWORD: get('DB_PASSWORD').required().asString(),
-
-
-}
+  PORT: get('PORT').default(3000).asPortNumber(),
+  PUBLIC_PATH: get('PUBLIC_PATH').default('public').asString(),
+  DB_URL: get('DB_URL').required().asString(),
+  DB_PASSWORD: get('DB_PASSWORD').required().asString(),
+  DB_NAME: get('DB_NAME').required().asString(),
+  DB_HOST: get('DB_HOST').required().asString(),
+  DB_PORT: get('DB_PORT').required().asPortNumber(),
+  DB_USERNAME: get('DB_USERNAME').required().asString(),
+  NODE_ENV: get('NODE_ENV').default('development').asString(),
+};
